4167d16e1b89720019f7b75a844268fd21e61d18
Abel Gaming <lcpdpolice4939@gmail.com>
2024-06-11 12:12:16 -0400

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,6 +1,6 @@
 fx_version 'cerulean'
 game 'gta5'
-description 'Abel Gaming - Standalone phone system for FiveM'
+description 'Abel Gaming - Standalone messaging phone system for FiveM'
 author 'Abel Gaming'
 version '1.0'
 lua54 'yes'
