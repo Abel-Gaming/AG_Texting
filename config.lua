2ca51d2ef12849989592996d7775985ff885240b
Abel Gaming <lcpdpolice4939@gmail.com>
2024-06-11 12:20:25 -0400

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,12 @@
+Config = {}
+
+-- Change the notification sound --
+--[[
+    To change the notification sound, go to the following link (https://wiki.gtanet.work/index.php?title=FrontEndSoundlist) and select whatever you want from the sound
+    soundName and SoundSetName columns. Edit them accordingly in the configuration section below!
+--]]
+Config.NotificationSoundName = 'CHALLENGE_UNLOCKED'
+Config.NotificationSoundSetName = 'HUD_AWARDS'
+
+Config.EnableDiscordLog = false -- This will allow you to manage and view all messages sent via a Discord Webhook
+Config.DiscordWebHook = ''
